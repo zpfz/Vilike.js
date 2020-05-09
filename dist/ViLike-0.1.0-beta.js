4b30c3665f0c8c10b0513b03b4677d1433f1f32f
zpfz <zpfz@seeyoz.cn>
2020-05-09 18:40:10 +0800

    fix: add ;
    Signed-off-by: zpfz <zpfz@seeyoz.cn>

--- dist/ViLike-0.1.0-beta.js
+++ dist/ViLike-0.1.0-beta.js
@@ -2,7 +2,7 @@
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-(function (root, factory) {
+;(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(factory);
   } else if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object') {
